8d7dfed8b8439bfb384354378f1d210ea4e1b10b
sebaszwenger <sebaszwenger@gmail.com>
2022-04-21 10:24:20 -0300

    Se creo un state que maneja el valor de la cita a editar. Se añadieron props a los componentes de Formulario y ListadoPacientes.

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,29 @@ import Header from "./components/Header";
 import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
+  //STATES
+  //State que maneja el arreglo de citas
   const [citas, setCitas] = useState([]);
+
+  //State que maneja la cita a editar
+  const [editarCita, setEditarCita] = useState({});
 
   return (
     <>
       <div className="w-5/6 mt-20 ml-auto mr-auto">
         <Header />
         <div className="mt-12 md:flex justify-center">
-          <Formulario citas={citas} setCitas={setCitas} />
-          <ListadoPacientes citas={citas} />
+          <Formulario
+            citas={citas}
+            setCitas={setCitas}
+            editarCita={editarCita}
+            setEditarCita={setEditarCita}
+          />
+          <ListadoPacientes
+            setCitas={setCitas}
+            citas={citas}
+            setEditarCita={setEditarCita}
+          />
         </div>
       </div>
     </>
