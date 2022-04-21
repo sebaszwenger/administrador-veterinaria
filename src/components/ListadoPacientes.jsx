9b7856cb6a3edb6c6b68fdd3dca55fef45c77f47
sebaszwenger <sebaszwenger@gmail.com>
2022-04-20 21:57:49 -0300

    se paso como props el state que maneja la cita a editar y a su vez se los pasa al componente de Paciente. Se agregaron los props de citas y set citas al componente de Paciente. Se muestra condicionalmente uno u otro encavezado de la lista de las citas dependiendo si hay o no alguna cita.

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,17 +1,41 @@
 import Paciente from "./Paciente";
 
-const ListadoPacientes = ({ citas }) => {
+const ListadoPacientes = ({ citas, setCitas, setEditarCita }) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen md:overflow-y-scroll pl-5 mb-5">
-      <h2 className="from-black text-3xl text-center">Listado Pacientes</h2>
-      <p className="text-xl mt-5 mb-10 text-center">
-        Administra tus {""}
-        <span className="text-indigo-600 font-bold ">Pacientes y citas</span>
-      </p>
+      {citas && citas.length ? (
+        <>
+          <h2 className="from-black text-3xl text-center">Listado Pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {""}
+            <span className="text-indigo-600 font-bold ">
+              Pacientes y citas
+            </span>
+          </p>
 
-      {citas.map((cita, index) => (
-        <Paciente key={index} cita={cita} />
-      ))}
+          {citas.map((cita) => (
+            <Paciente
+              key={cita.id}
+              cita={cita}
+              citas={citas}
+              setCitas={setCitas}
+              setEditarCita={setEditarCita}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">
+            Aun no hay pacientes
+          </h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Comienza agregando una cita {""}
+            <span className="text-indigo-600 font-bold ">
+              y apareceran en este lugar
+            </span>
+          </p>
+        </>
+      )}
     </div>
   );
 };
