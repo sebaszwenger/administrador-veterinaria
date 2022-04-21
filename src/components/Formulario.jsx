ea174e82fe8b92948f371ccf3f442f27650f60e1
sebaszwenger <sebaszwenger@gmail.com>
2022-04-21 10:22:12 -0300

    Se agregaron props del state que maneja la edicion de una cita. Se añadio un useEffect que revisa si hay una cita en edicion y rellenar el formulario. Se añadio una funcion que genera un id. Se agrego un map que reemplaza la cita a editar dentro del submit. Se añadio un condicional que cambia el valor del boton submit dependiendo de si se esta editando o creando una cita.

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 import Mensaje from "../components/Mensaje";
 
-const Formulario = ({ setCitas, citas }) => {
+const Formulario = ({ setCitas, citas, editarCita, setEditarCita }) => {
   //STATES
-  //Condicional que muestra un mensaje de error en pantalla
+  //State que maneja mensaje de error en pantalla
   const [error, setError] = useState(false);
 
   //State que maneja los campos del formulario
@@ -13,12 +13,27 @@ const Formulario = ({ setCitas, citas }) => {
     email: "",
     fecha: "",
     sintomas: "",
+    id: "",
   });
 
   // Extrae los valores del state paciente
-  const { mascota, email, propietario, fecha, sintomas } = paciente;
+  const { mascota, email, propietario, fecha, sintomas, id } = paciente;
+
+  useEffect(() => {
+    if (Object.keys(editarCita).length > 0) {
+      setPaciente(editarCita);
+    }
+  }, [editarCita]);
 
   //FUNCIONES
+  //Genera un id para el paciente
+  const generarId = () => {
+    const random = Math.random().toString(36).substring(2);
+    const fecha = Date.now().toString(36);
+
+    return random + fecha;
+  };
+
   //Valida los campos del formulario
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -29,11 +44,26 @@ const Formulario = ({ setCitas, citas }) => {
       return;
     }
 
-    //Borra mensaje de error
-    setError(false);
+    //Revisa si se esta queriendo editar una cita
+    if (Object.keys(editarCita).length > 0) {
+      //Edita una cita
+      const citasActualizadas = citas.map((cita) =>
+        cita.id === editarCita.id ? paciente : cita
+      );
+      setEditarCita({});
+      setCitas(citasActualizadas);
+    } else {
+      //Crea nueva cita
 
-    //Crea la cita del paciente
-    setCitas([...citas, paciente]);
+      //Genera un id para la cita
+      paciente.id = generarId();
+
+      //Borra mensaje de error
+      setError(false);
+
+      //Crea la cita del paciente
+      setCitas([...citas, paciente]);
+    }
 
     // Reiniciar el formulario
     setPaciente({
@@ -42,6 +72,7 @@ const Formulario = ({ setCitas, citas }) => {
       email: "",
       fecha: "",
       sintomas: "",
+      id: "",
     });
   };
 
@@ -60,7 +91,6 @@ const Formulario = ({ setCitas, citas }) => {
         Añade Pacientes y{" "}
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
-
       <form
         onSubmit={handleSubmit}
         className="bg-white shadow-md rounded-md py-10 p-5 mb-10"
@@ -138,9 +168,15 @@ const Formulario = ({ setCitas, citas }) => {
         <input
           type="submit"
           className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-800 rounded-md cursor-pointer transition-all"
-          value="Agregar Paciente"
+          // Cambia el value del boton segun se este creando o editando una cita
+          value={
+            Object.keys(editarCita).length
+              ? "Editar Paciente"
+              : "Agregar Paciente"
+          }
         />
       </form>
+      )
     </div>
   );
 };
